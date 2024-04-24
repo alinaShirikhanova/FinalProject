fa2ee4103a99b4c6e2db9e178005e5c532bafdce
alina <alina.meller.98@mail.ru>
2024-04-24 17:57:22 +0300

    notebook работает и таблица

--- session_window.py
+++ session_window.py
@@ -7,44 +7,44 @@ from database import Database
 class SessionWindow(Toplevel):
     def __init__(self):
         super().__init__()
-        self.geometry('500x500')
-        self.title("Добро пожаловать")
-        self.db = Database()
+        self.title('Post Service')
+        self.geometry('500x500+300+100')
 
-        # создаем виджет со вкладками
-        self.notebook = ttk.Notebook(self)
-        # создаем фреймы, на которых разместим списки пользователей и товаров
-        self.frame1 = Frame(self.notebook)
-        self.frame2 = Frame(self.notebook)
+        notebook = ttk.Notebook(self)
+        notebook.pack(expand=True, fill=BOTH)
+        frame1 = ttk.Frame(notebook)
+        frame1.pack()
+        notebook.add(frame1, text='Просмотр пользователей')
 
-        # создаем вкладки
-        self.notebook.add(self.frame1, text="Просмотр пользователей")
-        self.notebook.add(self.frame2, text="Просмотр товаров")
-        self.notebook.pack(expand=True, fill=BOTH)
+        frame2 = ttk.Frame(notebook)
+        frame2.pack()
+        notebook.add(frame2, text='Добавление пользователя')
 
-        # создаем переменную для списка пользователей
-        self.users_list = Variable()
-        # создаем таблицу списочную
-        self.users_listbox = Listbox(listvariable=self.users_list, master=self.frame1)
+        columns = ['id', 'login', 'password', 'name', 'surname', 'phone', 'email', 'birthdate', 'status']
+        self.users_table = ttk.Treeview(columns=columns, show='headings', master=frame1)
 
-        # создаем колоночки, задавая имена для обращения к ним внутри программы
-        self.columns = ['id', 'name', 'login', 'password', 'address']
+        columns_names = ['ID', 'Login', 'Password', 'Name', 'Surname', 'Phone', 'Email', 'Birthdate', 'Status']
+        columns_width = [30, 100, 100, 120, 120, 150, 150, 100, 100]
 
-        # создаем таблицу. show='headings' - тип отображения, отображаться будут имена столбцов в данном случае
-        self.users_table = ttk.Treeview(columns=self.columns, show='headings', master=self.frame1)
-
-        # названия столбцов таблицы
-        columns_names = ['ID', 'Name', 'Login', 'Password', 'Address']
-        columns_width = [30, 100, 100, 100, 130]
-
-        for i in range(len(self.columns)):
-            self.users_table.heading(self.columns[i], columns_names[i])
+        for i in range(len(columns)):
+            self.users_table.heading(columns[i], text=columns_names[i])
             self.users_table.column(f'#{i}', stretch=NO, width=columns_width[i])
 
+        vertical_scrollbar = ttk.Scrollbar(orient=VERTICAL, command=self.users_table.yview, master=frame1)
+        self.users_table.configure(yscrollcommand=vertical_scrollbar.set)
 
-        self.frame1.pack()
-        self.frame2.pack()
-        self.users_table.pack()
+        horizontal_scrollbar = ttk.Scrollbar(orient=HORIZONTAL, command=self.users_table.xview, master=frame1)
+        self.users_table.configure(xscrollcommand=horizontal_scrollbar.set)
+
+        self.load_users_button = ttk.Button(text='Обновить', master=frame1)
+
+        self.load_users_button.pack(side=BOTTOM, anchor=S)
+        vertical_scrollbar.pack(side=RIGHT, fill=Y)
+        self.users_table.pack(fill=BOTH, expand=True)
+        horizontal_scrollbar.pack(side=BOTTOM, fill=X)
+
+
+
 
 
 
